5766020a50eb7b88b4decfd4af254026d7b2775f
mariya <venediktovam@outlook.com>
2024-03-19 19:48:37 +0200

    fix da gripper - ofek + mariya

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 public class Constants {
     public static double wristFloorPos = 0.65, wristBackboardPos = 0.3;
     public static double leftGripperOpen = 0.75, leftGripperClose = 0.93;
-    public static double rightGripperOpen = 0.25, rightGripperClose = 0.48;
+    public static double rightGripperOpen = 0.52, rightGripperClose = 0.33;
     public static int initialArmPos = 2400, armPosFloor = 2350, armPosHang = 150, armPosBackdrop = -800;
     public static double ticks_in_degrees = (double) 360/(28*230*4*231);
     public static double p = 0.005, i = 0, d= 0.001, f = 0.225;
